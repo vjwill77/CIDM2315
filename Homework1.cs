d73441ad65c9ecb718c7d67c1473bb9f5c60999e
vjwill77 <vjwilliams1@buffs.wtamu.edu>
2024-08-29 11:06:52 -0500

    Homework1 code file

---
+++ Homework1.cs
@@ -0,0 +1,16 @@
+﻿namespace Homework1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //the code for the Homework1
+        double x = 2.5;
+        double y = 3.3;
+        double z = ((4*x*x)+(3*y));
+
+        Console.WriteLine($"X = {x}, Y = {y}");
+
+        Console.WriteLine($"The value of Z is: {z}");
+    }
+}
